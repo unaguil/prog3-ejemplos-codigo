1aa678d80c1da47a697c1b7e879bf5a68d45868b
Unai Aguilera <unai.aguilera@deusto.es>
2020-11-02 16:52:14 +0100

    Hanoi tower example added

---
+++ src/es/deusto/prog3/cap05/recursividad/ejercicios/TorreHanoi.java
@@ -0,0 +1,22 @@
+package es.deusto.prog3.cap05.recursividad.ejercicios;
+
+// Este programa resuelve el problema de la Torre de Hanoi
+// de manera recursiva utilizando recursividad múltiple.
+
+public class TorreHanoi {
+
+    private static void hanoiRec(int size, char source, char destiny, char auxiliary) {
+		if (size == 1) {  // Caso base
+			System.out.println("Mover disco 1 de " + source + " a " + destiny);
+		} else {
+			// Caso recursivo
+			hanoiRec(size - 1, source, auxiliary, destiny);
+			System.out.println("Mover disco " + size + " de " + source + " a " + destiny);
+			hanoiRec(size - 1, auxiliary, destiny, source);
+		}
+	}
+    
+    public static void main(String[] args) {
+        hanoiRec(3, 'A', 'C', 'B');
+    }
+}
