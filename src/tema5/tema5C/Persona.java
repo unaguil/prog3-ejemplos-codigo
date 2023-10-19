e571754100af27e221c840874b1b2e7375567914
Unai Aguilera <unai.aguilera@deusto.es>
2023-10-19 09:46:33 +0200

    Añadidos ejemplos de Tema5A y Tema5C

---
+++ src/tema5/tema5C/Persona.java
@@ -0,0 +1,42 @@
+package tema5.tema5C;
+
+import java.io.Serializable;
+
+// Ejemplo de clase serializable por Java
+// Implementa la interfaz necesaria
+
+public class Persona implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1339142877488259479L;
+    
+    // Los tipos primitivos son siempre serializables
+    private String name;
+    private String surname;
+    private int age;
+
+    public Persona(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Nombre: %s, Apellido: %s, Edad: %d", name, surname, age); 
+    }
+}
