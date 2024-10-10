9da1da7bd4f119ee11f05fe8e77c157ae72794e8
Unai Aguilera <unai.aguilera@deusto.es>
2024-10-10 19:23:00 +0200

    Added JTextArea events example

--- src/tema3/tema3A/EjemploJTextArea.java
+++ src/tema3/tema3A/EjemploJTextArea.java
@@ -1,63 +1,82 @@
 package tema3.tema3A;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JButton;
 import javax.swing.SwingUtilities;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 /**
  * Este ejemplo muestra cómo se utiliza un JTextArea
  */
 public class EjemploJTextArea extends JFrame {
 
-    /**
-     *
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
 
-    public EjemploJTextArea() {
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setTitle("Ejemplo Swing");
+	public EjemploJTextArea() {
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setTitle("Ejemplo Swing");
 
-        JTextArea textArea = new JTextArea();
-        textArea.setRows(15);
-        textArea.setColumns(30);
-        textArea.setLineWrap(true);
+		JTextArea textArea = new JTextArea();
+		textArea.setRows(15);
+		textArea.setColumns(30);
+		textArea.setLineWrap(true);
 
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        add(scrollPane, BorderLayout.CENTER);
+		textArea.getDocument().addDocumentListener(new DocumentListener() {
 
-        JButton printButton = new JButton("Imprimir texto seleccionado");
-        printButton.addActionListener(new ActionListener() {
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				System.out.println("Se ha borrado texto");
+			}
 
-            @Override
-            public void actionPerformed(ActionEvent arg0) {
-                String selectedText = textArea.getSelectedText();
-                System.out.println(selectedText);
-            }
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				System.out.println("Se ha insertado texto");
+			}
 
-        });
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				System.out.println("Se ha cambiado texto");
+			}
+		});
 
-        add(printButton, BorderLayout.SOUTH);
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		add(scrollPane, BorderLayout.CENTER);
 
-        pack();
-        setVisible(true);
-    }
+		JButton printButton = new JButton("Imprimir texto seleccionado");
+		printButton.addActionListener(new ActionListener() {
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String selectedText = textArea.getSelectedText();
+				System.out.println(selectedText);
+			}
 
-            @Override
-            public void run() {
-                new EjemploJTextArea();
-            }
+		});
 
-        });
-    }
+		add(printButton, BorderLayout.SOUTH);
+
+		pack();
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				new EjemploJTextArea();
+			}
+
+		});
+	}
 }
